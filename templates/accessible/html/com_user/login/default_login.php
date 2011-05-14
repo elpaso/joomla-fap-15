f04a28402a48c4e5c4390e3814a385f4ae24cbca
Alessandro Pasotti <ale@ale-genesi.(none)>
2011-05-14 15:23:29 +0200

    Fixed #1: pagina di login

--- templates/accessible/html/com_user/login/default_login.php
+++ templates/accessible/html/com_user/login/default_login.php
@@ -1,54 +1,72 @@
-<?php // @version $Id: default_login.php 36 2008-04-03 06:26:47Z elpaso $
-defined( '_JEXEC' ) or die( 'Restricted access' );
-?>
-<form action="<?php echo JRoute::_( 'index.php', true, $this->params->get('usesecure')); ?>" method="post" id="login" class="login_form<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
-	<?php if ( $this->params->get( 'page_title' ) ) : ?>
-	<h1 class="componentheading<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
-		<?php echo $this->params->get( 'header_login' ); ?>
-	</h1>
-	<?php endif; ?>
-
-	<?php if ( $this->params->get( 'description_login' ) || isset( $this->image ) ) : ?>
-		<div class="contentdescription<?php echo $this->params->get( 'pageclass_sfx' );?>">
-			<?php if (isset ($this->image)) :
-				echo $this->image;
-			endif;
-			if ($this->params->get('description_login')) : ?>
-			<p>
-				<?php echo $this->params->get('description_login_text'); ?>
-			</p>
-			<?php endif;
-			if (isset ($this->image)) : ?>
-			<div class="wrap_image">&nbsp;</div>
+<?php defined('_JEXEC') or die('Restricted access'); ?>
+<?php if(JPluginHelper::isEnabled('authentication', 'openid')) :
+		$lang = &JFactory::getLanguage();
+		$lang->load( 'plg_authentication_openid', JPATH_ADMINISTRATOR );
+		$langScript = 	'var JLanguage = {};'.
+						' JLanguage.WHAT_IS_OPENID = \''.JText::_( 'WHAT_IS_OPENID' ).'\';'.
+						' JLanguage.LOGIN_WITH_OPENID = \''.JText::_( 'LOGIN_WITH_OPENID' ).'\';'.
+						' JLanguage.NORMAL_LOGIN = \''.JText::_( 'NORMAL_LOGIN' ).'\';'.
+						' var comlogin = 1;';
+		$document = &JFactory::getDocument();
+		$document->addScriptDeclaration( $langScript );
+		JHTML::_('script', 'openid.js');
+endif; ?>
+<form action="<?php echo JRoute::_( 'index.php', true, $this->params->get('usesecure')); ?>" method="post" name="com-login" id="com-form-login">
+<table width="100%" border="0" align="center" cellpadding="4" cellspacing="0" class="contentpane<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
+<tr>
+	<td colspan="2">
+		<?php if ( $this->params->get( 'show_login_title' ) ) : ?>
+		<div class="componentheading<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
+			<?php echo $this->params->get( 'header_login' ); ?>
+		</div>
+		<?php endif; ?>
+		<div>
+			<?php echo $this->image; ?>
+			<?php if ( $this->params->get( 'description_login' ) ) : ?>
+				<?php echo $this->params->get( 'description_login_text' ); ?>
+				<br /><br />
 			<?php endif; ?>
 		</div>
-	<?php endif; ?>
-	<fieldset>
-		<div class="name">
-			<label for="user" ><?php echo JText::_( 'Username' ); ?></label>
-			<input name="username" type="text" class="inputbox" size="20"  id="user" />
-		</div>
-		<div class="pass">
-			<label for="pass" ><?php echo JText::_( 'Password' ); ?></label>
-			<input name="passwd" type="password" class="inputbox" size="20" id="pass" />
-		</div>
-		<div class="remember">
-			<label for="rem"><?php echo JText::_( 'Remember me' ); ?></label>
-			<input type="checkbox" name="remember" class="inputbox" value="yes" id="rem" />
-		</div>
-	</fieldset>
-	<p>
-		<a href="<?php echo JRoute::_( 'index.php?option=com_user&task=lostPassword' ); ?>">
-			<?php echo JText::_('Lost Password?'); ?></a>
-		<?php if ( $this->params->get( 'registration' ) ) : ?>
-		<?php echo JText::_('No account yet?'); ?>
-		<a href="<?php echo JRoute::_( 'index.php?option=com_user&task=register' ); ?>">
-			<?php echo JText::_( 'Register' ); ?></a>
-		<?php endif; ?>
-	</p>
+	</td>
+</tr>
 
-	<input type="submit" name="submit" class="button" value="<?php echo JText::_( 'Login' ); ?>" />
-	<noscript><?php echo JText::_( 'WARNJAVASCRIPT' ); ?></noscript>
+</table>
+<fieldset class="input">
+	<p id="com-form-login-username">
+		<label for="username"><?php echo JText::_('Username') ?></label><br />
+		<input name="username" id="username" type="text" class="inputbox" alt="username" size="18" />
+	</p>
+	<p id="com-form-login-password">
+		<label for="passwd"><?php echo JText::_('Password') ?></label><br />
+		<input type="password" id="passwd" name="passwd" class="inputbox" size="18" alt="password" />
+	</p>
+	<?php if(JPluginHelper::isEnabled('system', 'remember')) : ?>
+	<p id="com-form-login-remember">
+		<label for="remember"><?php echo JText::_('Remember me') ?></label>
+		<input type="checkbox" id="remember" name="remember" class="inputbox" value="yes" alt="Remember Me" />
+	</p>
+	<?php endif; ?>
+	<input type="submit" name="Submit" class="button" value="<?php echo JText::_('LOGIN') ?>" />
+</fieldset>
+<ul>
+	<li>
+		<a href="<?php echo JRoute::_( 'index.php?option=com_user&view=reset' ); ?>">
+		<?php echo JText::_('FORGOT_YOUR_PASSWORD'); ?></a>
+	</li>
+	<li>
+		<a href="<?php echo JRoute::_( 'index.php?option=com_user&view=remind' ); ?>">
+		<?php echo JText::_('FORGOT_YOUR_USERNAME'); ?></a>
+	</li>
+	<?php
+	$usersConfig = &JComponentHelper::getParams( 'com_users' );
+	if ($usersConfig->get('allowUserRegistration')) : ?>
+	<li>
+		<a href="<?php echo JRoute::_( 'index.php?option=com_user&view=register' ); ?>">
+			<?php echo JText::_('REGISTER'); ?></a>
+	</li>
+	<?php endif; ?>
+</ul>
+
 	<input type="hidden" name="option" value="com_user" />
 	<input type="hidden" name="task" value="login" />
 	<input type="hidden" name="return" value="<?php echo $this->return; ?>" />
